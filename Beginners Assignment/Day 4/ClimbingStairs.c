b98e86937ed5b12c147e310c68ea83c002e02148
Abiram007 <38308166+Abiram007@users.noreply.github.com>
2022-09-16 12:01:00 +0530

    Create ClimbingStairs.c

---
+++ Beginners Assignment/Day 4/ClimbingStairs.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n+1];
+    a[0] = a[1] = 1;
+    
+    for(int i=2; i<=n; i++)
+    a[i] = a[i-1] + a[i-2];
+ 
+    printf("%d",a[n]);
+}
