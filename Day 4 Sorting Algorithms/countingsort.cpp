fd663442adfc799233ea84100b3bea21f54975db
Abiram007 <38308166+Abiram007@users.noreply.github.com>
2022-10-21 15:17:13 +0530

    Create countingsort.cpp

---
+++ Day 4 Sorting Algorithms/countingsort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+void countSort(vector <int> &arr, int n)
+{
+    int mx = *max_element(arr.begin(), arr.end());
+    vector <int> freq(mx+1, 0);
+    vector <int> ans(n);
+    for(int i=0; i<n; i++)
+    {
+        freq[arr[i]] += 1;
+    }
+    for(int i=1; i<=mx; i++)
+    {
+        freq[i] = freq[i] + freq[i-1];
+    }
+    for(int i = n-1; i >= 0; i--)
+    {
+        ans[--freq[arr[i]]] = arr[i];
+    }
+    arr = ans;
+    return;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector <int> arr(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    countSort(arr, n);
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
