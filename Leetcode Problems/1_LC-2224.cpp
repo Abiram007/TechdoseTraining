b849d9772de1c63119fc831c972eba9343d867a1
Abiram007 <38308166+Abiram007@users.noreply.github.com>
2022-10-21 15:47:38 +0530

    Update 1_LC-2224.cpp

--- Leetcode Problems/1_LC-2224.cpp
+++ Leetcode Problems/1_LC-2224.cpp
@@ -7,9 +7,9 @@ public:
     int convertTime(string current, string correct) {
         int diff = getTime(correct) - getTime(current), ops[4] = {60,15,5,1}, ans = 0;
         for (int op : ops) {
-            ans += diff / op; //sooo....we checking how many times we gotta add op to current
-            diff %= op; //if we add that many times of op then wut remainder?? we find it by THIS!!!
+            ans += diff / op; 
+            diff %= op; 
         }
-        return ans; // TADAA!!!! BOOOM!!! we get no of ops
+        return ans;
     }
 };
